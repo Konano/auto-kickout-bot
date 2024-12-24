7591c52a020a7ffd41f068d7eb9a2d9a54043853
Konano <nanoapezlk@gmail.com>
2024-12-24 18:11:58 +0800

    minor: 移除 Sentry 配置中的实验性选项

--- bot.py
+++ bot.py
@@ -95,12 +95,12 @@ if 'SENTRY' in config and config['SENTRY'].get('dsn'):
         # Set traces_sample_rate to 1.0 to capture 100%
         # of transactions for tracing.
         traces_sample_rate=1.0,
-        _experiments={
-            # Set continuous_profiling_auto_start to True
-            # to automatically start the profiler on when
-            # possible.
-            "continuous_profiling_auto_start": True,
-        },
+        # _experiments={
+        #     # Set continuous_profiling_auto_start to True
+        #     # to automatically start the profiler on when
+        #     # possible.
+        #     "continuous_profiling_auto_start": True,
+        # },
     )
     shlr = SentryHandler()
     shlr.setLevel('WARNING')
